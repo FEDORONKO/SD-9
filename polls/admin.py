7d424113234bf519f8227d2828c07575b45e59d8
FEDORONKO <oleksandr.m.fedoronko@ukd.edu.ua>
2025-04-30 10:15:04 +0300

    Delete polls directory

--- polls/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Question
-
-# Реєструємо модель Question в адмінці
-admin.site.register(Question)
--- polls/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Question
-
-def question_list(request):
-    # отримуємо всі об’єкти Question
-    questions = Question.objects.all()
-    context = {'questions': questions}
-    # вказуємо шлях до шаблону polls/question_list.html
-    return render(request, 'polls/question_list.html', context)
